c5cf15eeacdb6bf2afec73463a57c13810b55241
entz95 <jentz6@protonmail.com>
2021-03-10 14:07:38 -0800

    added dungeon v2 a unity project that converts txt files into 2d sprite maps and exports a collision map and sprite map

---
+++ DungeonV2/Assets/Scripts/Cell.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Cell
+{
+    public GameObject gameObject;
+    public SpriteRenderer renderer;
+    public int X;
+    public int Y;
+
+    public Cell(GameObject prefab, Sprite sprite, int x, int y)
+    {
+        X = x;
+        Y = y;
+        gameObject = GameObject.Instantiate(prefab, Utils.GridToWorldPosition(x, y), Quaternion.identity);
+        renderer = gameObject.GetComponent<SpriteRenderer>();
+        gameObject.name = "X: " + x + "Y: " + y;
+        renderer.sprite = sprite;
+    }
+
+    public void UpdateTile(Sprite sprite)
+    {
+        renderer.sprite = sprite;
+    }
+}
