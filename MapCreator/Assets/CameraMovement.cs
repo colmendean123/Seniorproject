552603a284981d9af4ab22daaa364285747cac88
entz95 <jentz6@protonmail.com>
2021-06-01 11:55:05 -0700

    added camera movement using arrow keys and fixed the sprites not showing up

---
+++ MapCreator/Assets/CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    float speed = 0.5f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+        }
+    }
+}
---
+++ MapCreator/Assets/GridMap/Scripts/Main.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField] private TileMapVisual tileMapVisual;
+    private TileMap tilemap;
+    private TileMap.TileMapObject.TileMapSprite tileMapSprite;
+
+    private void Start()
+    {
+        int width = 0, height = 0;
+        width = 15; height = 15;
+        tilemap = new TileMap(width, height, 7.5f, new Vector3(-(width *3), -(height * 3)));
+        tilemap.setTileMapVisual(tileMapVisual);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Utils.GetMouseWorldPosition();
+            tilemap.SetTileMapSprite(mousePosition, tileMapSprite);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            tileMapSprite = TileMap.TileMapObject.TileMapSprite.None;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            tileMapSprite = TileMap.TileMapObject.TileMapSprite.Wall;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            tileMapSprite = TileMap.TileMapObject.TileMapSprite.Dirt;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            tileMapSprite = TileMap.TileMapObject.TileMapSprite.Grass;
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            tileMapSprite = TileMap.TileMapObject.TileMapSprite.Water;
+        }
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            tilemap.SaveCollisionMap();
+            Utils.TextPopupMouse("Saved Collision Map");
+        }
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            tilemap.SaveSpriteMap();
+            Utils.TextPopupMouse("Saved Sprite Map");
+        }
+    }
+
+}
