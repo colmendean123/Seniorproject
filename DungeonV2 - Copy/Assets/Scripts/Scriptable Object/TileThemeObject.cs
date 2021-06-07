622e09ccb016043597a2a9db3759d66ce5247f1c
entz95 <57931499+entz95@users.noreply.github.com>
2021-06-06 19:19:59 -0700

    Delete DungeonV2 - Copy directory

--- DungeonV2 - Copy/Assets/Scripts/Scriptable Object/TileThemeObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Tile Theme", menuName = "Tiles/Theme")]
-public class TileThemeObject : ScriptableObject
-{
-    public Sprite[] tiles;
-}
